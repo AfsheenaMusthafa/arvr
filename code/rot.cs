7c37817e4e7c64fe8df0dfeb68738a78b124bf74
Afsheena Musthafa <afshimusthu433@gmail.com>
2025-10-20 22:26:58 +0530

    Update rot.cs

--- code/rot.cs
+++ code/rot.cs
@@ -1,1 +1,12 @@
+using UnityEngine;
 
+public class RotateObject : MonoBehaviour
+{
+    public float rotationSpeed = 50f; // Rotation speed in degrees per second
+
+    void Update()
+    {
+        // Rotate the object continuously around Y-axis
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+}
