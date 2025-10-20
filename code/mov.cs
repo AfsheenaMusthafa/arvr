88c806a9a66b6c39f7861f0a916febabf1fd2eb9
Afsheena Musthafa <afshimusthu433@gmail.com>
2025-10-20 22:26:00 +0530

    Update mov.cs

--- code/mov.cs
+++ code/mov.cs
@@ -1,1 +1,18 @@
+using UnityEngine;
+
+public class MoveCharacter : MonoBehaviour
+{
+    public float moveSpeed = 5f; // Movement speed
+
+    void Update()
+    {
+        // Get input from keyboard
+        float moveX = Input.GetAxis("Horizontal"); // Left/Right or A/D
+        float moveZ = Input.GetAxis("Vertical");   // Forward/Backward or W/S
+
+        // Move the character in world space
+        Vector3 move = new Vector3(moveX, 0, moveZ) * moveSpeed * Time.deltaTime;
+        transform.Translate(move, Space.World);
+    }
+}
 
